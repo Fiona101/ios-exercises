1c7c51b5b1af72857d335a88346d614bcec4ecfb
Fiona Alpe <fiona.alpe@gmail.com>
2016-03-06 12:25:23 +0300

    Assignment 8: Numbers and Variables Completed

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -11,18 +11,18 @@
 @implementation SimpleCalculator
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
-    /* WORK HERE */
-    return number;
+    /* Change so that test gives the number 18 */
+    return number + 1;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
-    /* WORK HERE */
-    return number1;
+    /* Change to give a number of 20 so the addition test method will pass */
+    return number1 + number2;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
-    /* WORK HERE */
-    return 0;
+    /* Change so that the modulus shows a remainder of 3 */
+    return dividend % divisor;
 }
 
 @end
