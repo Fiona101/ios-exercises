6e6d6fd55eec0fa492df69588b0997d94ff08888
Fiona Alpe <fiona.alpe@gmail.com>
2016-04-01 21:23:59 +0300

    Assignment 10: Equality tests successfully completed

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -12,55 +12,33 @@
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
+    BOOL isSame = NO;
     
+    isSame = [string1 isEqualToString:string2];
     /* any * class must be set to 0 or it will return a null or NO value /*/
     
-    NSString *cerealString = nil;
-    /*/ NSString *identicalCerealString = nil;/*/
-    
-    /*/ now set the identical string to the same as cerealString which is assigned this value in the test method /*/
-    /*/ this works fir the first test NSString *identicalCerealString = [[NSString alloc] initWithFormat:@"Cheerios"];/*/
-    
-    /* NSString *isTheSameAsString = [[NSString alloc] initWithFormat:@"Cheerios"];/*/
-    /*/ NSString *isTheSameAsString = nil;/*/
-    NSString *identicalCerealString = [[NSString alloc] initWithFormat:@"Cheerios"];
-    NSString *secondCerealString = nil;
-    
-    /*/ return isTheSameAsString;/*/
-    return identicalCerealString , secondCerealString;
-    
-    /*/ this return works for the first test return identicalCerealString;/*/
-     /*/
-    
-    NSString *cerealString = nil;
-    NSString *identicalCerealString = nil;
-    *secondCerealString = @"Trix";
-    NSString *isTheSameAsString = [cerealString isEqual: identicalCerealString];
-    return isTheSameAsString;
-    /*/
-     
+    return isSame;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-    /* WORK HERE */
     
-    /*/ NSNumber *sixNumber = [NSNumber numberWithInt:6]; /*/
-    /*/ NSNumber *sixNumber = nil;
-    NSNumber *identicalSixNumber = nil; /*/
-    NSNumber *sixNumber = [NSNumber numberWithFloat:6];
-    NSNumber *identicalSixNumber = sixNumber;
-    NSNumber *sevenNumber = nil;
+    BOOL isSameNumber;
     
-    return sixNumber , identicalSixNumber , sevenNumber;
+    isSameNumber = [number1 isEqualToNumber:number2];
+    
+    return isSameNumber;
+    
     
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
     
-    NSInteger isGreaterThan = (7 > 4);
+    BOOL comparativeOperator;
     
-    return isGreaterThan;
+    comparativeOperator = (integer1 > integer2);
+    
+    return comparativeOperator;
 }
 
 @end
