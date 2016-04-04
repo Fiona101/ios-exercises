2ffc5269e1bf5f43970db735432a12765c280066
Fiona Alpe <fiona.alpe@gmail.com>
2016-04-04 23:32:19 +0300

    Assignment 11 Not Successful

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,20 +15,44 @@
 
     /* WORK HERE */
     
+    if (dollars < 4) {
+        itemToReturn = @"get out of my store";
+    } else if ((dollars = 4)) {
+        itemToReturn = @"If Mary has $4, she should be told to get out.";
+    } else if ((dollars > 4) && (dollars < 5)) {
+        itemToReturn = @"have some gum";
+    } else if ((dollars = 5)) {
+        itemToReturn = @"If Mary has $5, she should receive an offer to purchase gum.";
+    } else if ((dollars > 5) && (dollars < 6)) {
+        itemToReturn = @"have an apple";
+    } else if ((dollars = 6)) {
+        itemToReturn = @"If Mary has $6, she should receive an offer to purchase an apple.";
+    } else if ((dollars >= 1000) && ( dollars < 1000000000)) {
+        itemToReturn = @"have an Apple computer";
+    } else if ((dollars = 1000000000)) {
+        itemToReturn = @"have The Big Apple";
+    } else {
+        itemToReturn = @"Last catchall";
+    }
+
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
-}
-
+  
+    
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
-    /* WORK HERE */
+    
+    /*/ WORK HERE /*/
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
+    cost = (getsDiscount = YES) ? 18 : 24
+    
+   /*/ if (self.getsDiscount) {
         cost *= .75;
-    }
+    } /*/
+    
+}
     
     return cost;
-}
-
 @end
+    
