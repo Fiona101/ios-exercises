e683f324abc9a484e01ca6d1b58f28cba737b76c
Fiona Alpe <fiona.alpe@gmail.com>
2016-04-06 19:08:16 +0300

    Assignment 11 Successful

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,21 +15,26 @@
 
     /* WORK HERE */
     
-    if (dollars < 4) {
+    if (dollars <= 4) {
         itemToReturn = @"get out of my store";
-    } else if ((dollars = 4)) {
-        itemToReturn = @"If Mary has $4, she should be told to get out.";
-    } else if ((dollars > 4) && (dollars < 5)) {
+//    } else if ((dollars = 4)) {
+//        itemToReturn = @"If Mary has $4, she should be told to get out.";
+        
+    } else if (dollars == 5) {
         itemToReturn = @"have some gum";
-    } else if ((dollars = 5)) {
-        itemToReturn = @"If Mary has $5, she should receive an offer to purchase gum.";
-    } else if ((dollars > 5) && (dollars < 6)) {
+        
+   // } else if ((dollars = 5)) {
+    //    itemToReturn = @"If Mary has $5, she should receive an offer to purchase gum.";
+    
+    } else if (dollars >= 6 && dollars < 1000) {
         itemToReturn = @"have an apple";
-    } else if ((dollars = 6)) {
-        itemToReturn = @"If Mary has $6, she should receive an offer to purchase an apple.";
+        
+   // } else if ((dollars >= 6) && (dollars < 1000)) {
+     //   itemToReturn = @"If Mary has $6, she should receive an offer to purchase an apple.";
+        
     } else if ((dollars >= 1000) && ( dollars < 1000000000)) {
         itemToReturn = @"have an Apple computer";
-    } else if ((dollars = 1000000000)) {
+    } else if ((dollars >= 1000000000)) {
         itemToReturn = @"have The Big Apple";
     } else {
         itemToReturn = @"Last catchall";
@@ -37,22 +42,21 @@
 
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
-  
-    
-- (NSUInteger) dollarCostForAppleFlavoredVodka {
+}
+
+- (NSUInteger)dollarCostForAppleFlavoredVodka {
     
     /*/ WORK HERE /*/
 
     NSUInteger cost = 24;
     
-    cost = (getsDiscount = YES) ? 18 : 24
+    cost = (self.getsDiscount == YES) ? 18 : 24;
     
    /*/ if (self.getsDiscount) {
         cost *= .75;
     } /*/
     
-}
-    
     return cost;
+}
 @end
     
