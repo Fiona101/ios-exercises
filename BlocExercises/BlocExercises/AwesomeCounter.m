ad8ca26312dc3266e4858b2d8627f570c570be14
Fiona Alpe <fiona.alpe@gmail.com>
2016-04-11 22:14:07 +0300

    Assignment 12 unsuccessful completion

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,31 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
+    
     /* WORK HERE */
-    return @"";
+    
+    NSMutableString *counter = nil;
+   
+    if (number > otherNumber) {
+      
+    NSMutableString *counter = @(otherNumber);
+        
+        for (NSInteger otherNumber = otherNumber; otherNumber < number; otherNumber++) {
+            
+        [counter appendString:[@(otherNumber) stringValue]];
+        }
+        
+    } else {
+        
+    NSMutableString *counter = @(number);
+        
+        for (NSInteger number = number; number < otherNumber; number++) {
+        
+        [counter appendString:[@(otherNumber) stringValue]];
+        
+    }
+    return counter;
+    }
 }
-
 @end
+
