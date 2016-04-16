73aaadd18cb5d988cbc4c1d09934943950430a63
Fiona Alpe <fiona.alpe@gmail.com>
2016-04-16 21:48:57 +0300

    Assignment 13 all but the last test successful

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,97 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    
+NSArray *starTrekArrays =[characterString componentsSeparatedByString:@";"];
+    
+    return starTrekArrays;
+    /*/ this method is successful /*/
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    
+    NSString *starTrekString = [characterArray componentsJoinedByString:@";"];
+    
+    return starTrekString;
+    
+    /*/ this method is successful /*/
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *returnArray_sorted = [[NSMutableArray alloc]init];
+    
+    [returnArray_sorted  addObjectsFromArray: [characterArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)]];
+    
+    
+    return returnArray_sorted;
+    
+   /*/ this method is successful /*/
 }
+
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
-    return NO;
+    
+    BOOL boolReturn = NO;
+    
+    NSMutableArray *searchArray = [[NSMutableArray alloc]init];
+    
+    searchArray = [NSMutableArray arrayWithArray:[@[characterArray] mutableCopy]];
+    
+    NSPredicate *worfPredicate = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c]'Worf'"];
+    
+    
+    /*/ searchArray = [characterArray filteredArrayUsingPredicate:worfPredicate];/*/
+    
+    /*/ searchArray = [NSMutableArray arrayWithArray:characterArray];/*/
+    
+    /*/ NSPredicate *worfPredicate = [NSPredicate predicateWithFormat:@"SELF contains[c]'Worf'"];/*/
+    
+    /*/ NSUInteger arrayLength = 0;/*/
+    
+    /*/ NSMutableArray *searchArray = [[NSMutableArray alloc]init];
+    
+    [searchArray  addObjectsFromArray: [characterArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)]];
+    
+    NSPredicate *worfPredicate = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c]'Worf'"];/*/
+    
+    [searchArray filterUsingPredicate:worfPredicate];
+    
+    /*/ boolReturn = [worfPredicate evaluateWithObject:[searchArray]]; /*/
+    
+    /*/ [searchArray evaluateWithObject:worfPredicate]; /*/    
+    /*/ if {/*/
+    
+   /*/ [searchArray filterUsingPredicate:worfPredicate];/*/
+    
+    
+    /*/ arrayLength = [searchArray count];
+    
+    if ((arrayLength = 0)) {
+        
+        boolReturn = NO;
+        
+    } else {
+        boolReturn = NO;
+    }/*/
+    
+    if ((worfPredicate = nil)) {
+        
+        boolReturn = NO;
+        
+    } else {
+        
+        boolReturn = YES;
+    }
+        
+        
+
+    /*/ BOOL boolReturn = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'Worf'"]; /*/
+    
+    return boolReturn;
 }
 
 @end
