515e30ecb379924230c12a7b8cb259c2ec0fa0bc
Fiona Alpe <fiona.alpe@gmail.com>
2016-04-27 18:58:41 +0300

    Assignment-15-data-successful

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,61 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
-}
+    
+
+    int doubleTheNumber = [number intValue] * 2;
+    
+    NSNumber *returnDoubleNumber = [NSNumber numberWithInt:doubleTheNumber];
+    
+    return returnDoubleNumber;
+
+    }
+
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *returnArray = [[NSMutableArray alloc] init];
+    
+    if (number <= otherNumber) {
+        
+        for (NSInteger i = number; i <= otherNumber; i++) {
+            
+            NSNumber *convertedNumber = [NSNumber numberWithLong:i];
+            
+            [returnArray addObject:convertedNumber]; }
+        
+    } else {
+        
+        for (NSInteger i = otherNumber; i <= number; i++) {
+            
+            NSNumber *convertedNumber = [NSNumber numberWithLong:i];
+            
+            [returnArray addObject:convertedNumber]; }
+        
+    }
+    
+    return returnArray;
+    
 }
+
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+// the below single line gives the same successful result more elegantly
+// return [[arrayOfNumbers valueForKeyPath:@"@min.self"] integerValue];
+    
+    NSInteger lowestInteger;
+    
+    NSNumber* lowestNumber;
+    
+    
+    lowestNumber = [arrayOfNumbers valueForKeyPath:@"@min.self"];
+    
+    
+    lowestInteger = [lowestNumber integerValue];
+    
+    return lowestInteger;
 }
 
 @end
